5d839f03cbd783d5fe24861f5334402e66fff180
SpaceScout <nelindanila7@gmail.com>
2024-09-01 02:05:28 +0300

    123

--- dw_backend/subscription/urls.py
+++ dw_backend/subscription/urls.py
@@ -3,5 +3,5 @@ from subscription import views
 
 urlpatterns = [
     path('activate_key/', views.KeyActivationsView.as_view(), name='activate_key'),
-    path('new_keys/', views.NewKeys.as_view(), name='new_keys'),
+    # path('new_keys/', views.NewKeys.as_view(), name='new_keys'),
 ]
