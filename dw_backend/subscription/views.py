0650210b13fa1fe2e1f98232f1239ba1e1decde8
SpaceScout <nelindanila7@gmail.com>
2024-09-01 02:09:06 +0300

    ?

--- dw_backend/subscription/views.py
+++ dw_backend/subscription/views.py
@@ -24,17 +24,17 @@ class KeyActivationsView(APIView):
         except ValidationError as e:
             return JsonResponse({'error': str(e)}, status=400)
 
-# class NewKeys(APIView):
+class NewKeys(APIView):
 
-#     permission_classes = [IsAuthenticated]
-#     authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
-#     def post(self, request):
-#         key_number = request.data.get('number')
-#         user = request.user
+    def post(self, request):
+        key_number = request.data.get('number')
+        user = request.user
 
-#         try:
-#             # SubscriptionKey.objects.activate_sub_key(key=key, user=user)
-#             return JsonResponse({'message': 'Key activated!'}, status=200)
-#         except ValidationError as e:
-#             return JsonResponse({'error': str(e)}, status=400)
+        try:
+            # SubscriptionKey.objects.activate_sub_key(key=key, user=user)
+            return JsonResponse({'message': 'Key activated!'}, status=200)
+        except ValidationError as e:
+            return JsonResponse({'error': str(e)}, status=400)
