44e7da1ea1ba57850204c42b64203fb791ed88f4
SpaceScout <nelindanila7@gmail.com>
2024-09-04 20:20:15 +0300

    ASDASDSAD

--- dw_backend/dw_backend/wsgi.py
+++ dw_backend/dw_backend/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dw_backend.dw_backend.settings')
+import sys
+sys.path.append('/dw_backend')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dw_backend.settings')
 
 application = get_wsgi_application()
